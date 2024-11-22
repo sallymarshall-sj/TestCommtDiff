97df1be5ee127508cd46a3a7ab57579d9d06d155
Matthew O'Dea <matthewodea@ip-192-168-1-204.ec2.internal>
2024-11-22 14:27:43 +1100

    exec js with comments

---
+++ exec.js
@@ -0,0 +1,126 @@
+const { execSync } = require('child_process');
+const yargs = require('yargs');
+const compact = require('lodash/compact');
+const _ = require('lodash');
+const color = {
+    fgBlue: '\x1b[34m',  // Blue foreground color ANSI escape code
+    reset: '\x1b[0m',      // Reset ANSI escape code
+};
+
+const ticketPatterns = ['AFE-', 'RSB-', 'SPB-', 'TABT-']; // App project tags
+const ticketRegExp = /\b[A-Z]+-\d+\b/g; // Regular expression to match JIRA ticket numbers
+
+// get all tickets that have been worked on in a specific set of commits
+// ASSUMPTION - all commits are tagged with the ticket that they are relevant to. Is this automatic or manual? What happens to commits that aren't tagged?
+const extractTicketsFromCommits = commits => {
+    const ticketsList = [];
+    commits.forEach(commitMessage => {
+        const matches = commitMessage.match(ticketRegExp);
+        if (matches) {
+            matches.forEach(match => {
+                if (!ticketsList.includes(match)) {
+                    ticketsList.push(match);
+                }
+            });
+        }
+    });
+    return ticketsList;
+};
+
+
+// worker app -> create specific release branches
+// branch off develop branch
+// 
+const getUniqueTicketsBetweenBranches = (targetBranch, sourceBranch, repositoryPath) => {
+    execSync(`git -C ${repositoryPath} fetch`);
+    const cherryPickedCommits = execSync(`git -C ${repositoryPath} log --cherry-pick --oneline origin/${targetBranch} ^origin/${sourceBranch}`)
+        // get all commits that are in target branch, but not source branch. What does cherry pick do?
+        // cherry pick is used to grab individual commits from dev branch (which contains all up to date code)
+        .toString()
+        .trim()
+        .split('\n');
+    const allCommits = execSync(`git -C ${repositoryPath} log --oneline origin/${targetBranch}..origin/${sourceBranch}`)
+        // get all commits that are in target branch, but not in source branch? .. operator seems like in A but not in B. Why use that here, and ^ earlier.
+        // how is this different to the cherry picked commits?
+        .toString()
+        .trim()
+        .split('\n');
+    const allTickets = extractTicketsFromCommits(allCommits); // get all tickets
+    const cherryPickedTickets = extractTicketsFromCommits(cherryPickedCommits); // get cherry picked tickets (figure out what these are)
+    const newTickets = _.difference(allTickets, cherryPickedTickets); // find any new tickets that did not exist before. What happens to badly tagged commits here?
+    // ie its valid as per the Regex starts with, but the number is made up or invalid or matches an old ticket? Do we care? Does this ever happen
+    const validTickets = newTickets.filter(ticket => ticketPatterns.some(pattern => ticket.startsWith(pattern)));
+    return validTickets.sort();
+};
+
+
+/**
+ * Example script
+ * npm run app-tickets -- --sourceBranch=develop --targetBranch=worker-app-5.1.0-RP6165_RSPP --type=list
+ */
+const argv = yargs // how does yargs works
+    .option('targetBranch', {
+        description: 'The name of the target branch',
+        type: 'string',
+        demandOption: true,
+    })
+    .option('sourceBranch', {
+        description: 'The name of the source branch',
+        type: 'string',
+        default: 'develop',
+        demandOption: false,
+    })
+    .option('responseType', {
+        description: 'The type of the response',
+        type: 'string',
+        demandOption: false,
+        default: 'jira',
+        choices: ['jira', 'list'],
+    }).argv;
+
+
+const getAllCommits = () => {
+    const {
+        targetBranch,
+        sourceBranch,
+        responseType,
+    } = argv || {};
+    if (!targetBranch || !sourceBranch || !responseType) {
+        console.error(
+            `Please provide all the params of this script. Missing ${compact([
+                !targetBranch && '--targetBranch',
+                !sourceBranch && '--sourceBranch',
+                !responseType && '--responseType'
+            ]).join(', ')}`
+        );
+        process.exit(1);
+    }
+
+    console.log(`-> ${color.fgBlue}Fetching commits of worker app${color.reset}`);
+    const appTickets = getUniqueTicketsBetweenBranches(targetBranch, sourceBranch, './');
+
+    console.log(`-> ${color.fgBlue}Fetching commits of partner resources${color.reset}`);
+    const partnerResourcesSourceBranch = sourceBranch !== 'develop' ? sourceBranch : 'worker/merged'; // what is the partner resource branch ?? 
+    const resourceTickets = getUniqueTicketsBetweenBranches(targetBranch, partnerResourcesSourceBranch, '../frontend-partner-resources');
+
+    console.log(`-> ${color.fgBlue}Fetching commits of environment configurations${color.reset}\n`);
+    const envConfigsSourceBranch = sourceBranch !== 'develop' ? sourceBranch : 'worker/merged'; // why do we need configurations seperately, what is in this 
+    const configTickets = getUniqueTicketsBetweenBranches(targetBranch, envConfigsSourceBranch, '../frontend-environment-configurations');
+    const totalTickets = _.uniqWith([...appTickets, ...resourceTickets, ...configTickets], _.isEqual);
+
+    if (responseType === 'list') {
+        execSync(`echo ${totalTickets.join(' ')} | pbcopy`); //
+    } else {
+        const joiner = '%2C%20'; // why?
+        const url = `https://swipejobs.atlassian.net/issues/?jql=issueKey%20in%20(${totalTickets.join(joiner)})`;
+        const quotedUrl =`"${url}"`;
+        execSync(`echo ${quotedUrl} | pbcopy`); // does pbcopy just put in clipboard so you run this script then head to jira and paste
+    }
+
+    console.log(`-> ${color.fgBlue}List of tickets: ${color.reset}${totalTickets.join(', ')}`);
+    console.log(`-> ${color.fgBlue}Number of tickets: ${color.reset}${totalTickets.length}${color.reset}`);
+};
+
+getAllCommits();
+
+// Updates to a ticket that has already been tagged and included in release, but maybe not updated version
